b2794f86a05f1c986c348850c63ee8356c326538
Michael Saelim <msaelim@grubhub.com>
2015-06-26 07:51:58 -0500

    writing harvester, with some of the retry logic in place

--- src/main/java/io/github/mikesaelim/arxivoaiharvester/ArxivOAIHarvester.java
+++ src/main/java/io/github/mikesaelim/arxivoaiharvester/ArxivOAIHarvester.java
@@ -9,7 +9,9 @@ import io.github.mikesaelim.arxivoaiharvester.model.response.GetRecordResponse;
 import io.github.mikesaelim.arxivoaiharvester.model.response.ListRecordsResponse;
 import io.github.mikesaelim.arxivoaiharvester.xml.ParsedXmlResponse;
 import io.github.mikesaelim.arxivoaiharvester.xml.XMLParser;
+import lombok.Data;
 import lombok.NonNull;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -19,6 +21,7 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
 
 /**
  * TODO javadoc
@@ -31,12 +34,42 @@ public class ArxivOAIHarvester {
     private final CloseableHttpClient httpClient;
     private final XMLParser xmlParser;
 
+    private final int maxNumRetries;
+    private final Duration minWaitBetweenRequests;
+    private final Duration maxWaitBetweenRequests;
+
     private final String userAgentHeader;
     private final String fromHeader;
 
+    /**
+     * Construct a harvester with the default settings:
+     * <ul>
+     *     <li>Maximum number of retries: 3</li>
+     *     <li>Minimum wait time between requests: 10 seconds</li>
+     *     <li>Maximum wait time between requests: 5 minutes</li>
+     * </ul>
+     */
     public ArxivOAIHarvester(CloseableHttpClient httpClient, String userAgentHeader, String fromHeader) {
-        // TODO add harvester control flow params
+        this(httpClient, 3, Duration.ofSeconds(10), Duration.ofMinutes(5), userAgentHeader, fromHeader);
+    }
+
+    public ArxivOAIHarvester(@NonNull CloseableHttpClient httpClient,
+                             int maxNumRetries,
+                             @NonNull Duration minWaitBetweenRequests,
+                             @NonNull Duration maxWaitBetweenRequests,
+                             String userAgentHeader,
+                             String fromHeader) {
+        if (maxNumRetries < 0) {
+            throw new IllegalArgumentException("Maximum number of retries must be 0 or greater");
+        }
+        if (minWaitBetweenRequests.compareTo(maxWaitBetweenRequests) > 0) {
+            throw new IllegalArgumentException("Minimum wait time between requests must be less than or equal to maximum");
+        }
+
         this.httpClient = httpClient;
+        this.maxNumRetries = maxNumRetries;
+        this.minWaitBetweenRequests = minWaitBetweenRequests;
+        this.maxWaitBetweenRequests = maxWaitBetweenRequests;
         this.userAgentHeader = userAgentHeader;
         this.fromHeader = fromHeader;
 
@@ -93,10 +126,12 @@ public class ArxivOAIHarvester {
      * @return parsed content of the response from the repository
      *
      * @throws NullPointerException if requestUri is null
-     * @throws BadArgumentException if the repository's response contains a BadArgument error
-     * @throws BadResumptionTokenException if the repository's response contains a BadResumptionToken error
+     * @throws HttpException if there is a problem communicating with the repository
+     * @throws TimeoutException if there have been too many retries, or the repository has suggested a wait time that is too long
      * @throws ParseException if parsing fails
      * @throws RepositoryError if the repository's response was parseable but invalid
+     * @throws BadArgumentException if the repository's response contains a BadArgument error
+     * @throws BadResumptionTokenException if the repository's response contains a BadResumptionToken error
      */
     private ParsedXmlResponse harvest(@NonNull URI requestUri) {
         // TODO control flow and 503
@@ -105,54 +140,101 @@ public class ArxivOAIHarvester {
         httpRequest.addHeader("User-Agent", userAgentHeader);
         httpRequest.addHeader("From", fromHeader);
 
-        log.info("Sending request to arXiv OAI repository: {}", requestUri);
+        int numRetries = 0;
+        while (numRetries <= maxNumRetries) {
+            RepositoryResponse response = tryHarvest(httpRequest);
+            if (response.getParsedXmlResponse() != null) {
+                return response.getParsedXmlResponse();
+            } else {
+                Duration wait = response.getWait();
 
-        ParsedXmlResponse parsedXmlResponse;
+                if (wait.compareTo(maxWaitBetweenRequests) > 0) {
+                    String errorString = "Repository-suggested wait time of " + wait +
+                            "exceeds maximum allowed wait time of " + maxWaitBetweenRequests +
+                            "; aborting request " + requestUri;
+                    log.warn(errorString);
+                    throw new TimeoutException(errorString);
+                } else if (wait.compareTo(minWaitBetweenRequests) < 0) {
+                    wait = minWaitBetweenRequests;
+                }
+
+                // TODO waiting
+            }
+
+            numRetries++;
+        }
+
+        String errorString = "Too many retries; aborting request " + requestUri;
+        log.warn(errorString);
+        throw new TimeoutException(errorString);
+    }
+
+
+    /**
+     * Send the harvesting request to the arXiv OAI repository once.
+     */
+    private RepositoryResponse tryHarvest(HttpGet httpRequest) {
+        log.info("Sending request to arXiv OAI repository: {}", httpRequest.getURI());
+
         try (CloseableHttpResponse httpResponse = httpClient.execute(httpRequest)) {
             int httpStatusCode = httpResponse.getStatusLine().getStatusCode();
 
             switch (httpStatusCode) {
                 case HttpStatus.SC_OK:
-                    log.info("Parsing response from arXiv OAI repository for request {}", requestUri);
+                    log.info("Parsing response from arXiv OAI repository for request {}", httpRequest.getURI());
 
+                    ParsedXmlResponse parsedXmlResponse;
                     try {
                         parsedXmlResponse = xmlParser.parse(httpResponse.getEntity().getContent());
                     } catch (BadArgumentException | BadResumptionTokenException e) {
-                        log.error(String.format("Repository complained about input for request %s", requestUri), e);
+                        log.error(String.format("Repository complained about input for request %s", httpRequest.getURI()), e);
                         throw e;
                     } catch (ParseException | RepositoryError e) {
-                        log.error(String.format("Error parsing response for request %s", requestUri), e);
+                        log.error(String.format("Error parsing response for request %s", httpRequest.getURI()), e);
                         throw e;
                     }
 
-                    log.info("Response parsed for request {}", requestUri);
+                    log.info("Response parsed for request {}", httpRequest.getURI());
 
-                    return parsedXmlResponse;
+                    return new RepositoryResponse(parsedXmlResponse, null);
                 case HttpStatus.SC_MOVED_TEMPORARILY:
                     // Handling this is not currently supported
-                    String movedErrorString = "Redirect received for request " + requestUri;
+                    String movedErrorString = "Redirect received for request " + httpRequest.getURI();
                     log.error(movedErrorString);
                     throw new UnsupportedRedirectException(movedErrorString);
                 case HttpStatus.SC_NOT_FOUND:
-                    String notFoundErrorString = "Received 404 for request " + requestUri;
+                    String notFoundErrorString = "Received 404 for request " + httpRequest.getURI();
                     log.error(notFoundErrorString);
                     throw new RepositoryError(notFoundErrorString);
                 case HttpStatus.SC_SERVICE_UNAVAILABLE:
                     // TODO: Handle 503
+                    log.info(""); // TODO
+                    return new RepositoryResponse(null, Duration.ofSeconds(20)); // TODO
                 default:
                     // Unfortunately, we currently aren't prepared to handle other HTTP status codes.  The OAI specs
                     // don't really say what to do for most of them.  So we log a warning and return an error response.
-                    String defaultErrorString = "Returned status code " + httpStatusCode + ": " +
+                    String defaultErrorString = "Request to arXiv OAI repository " + httpRequest.getURI() +
+                            " returned status code " + httpStatusCode + ": " +
                             httpResponse.getStatusLine().getReasonPhrase() + ": " +
                             EntityUtils.toString(httpResponse.getEntity());
                     log.error(defaultErrorString);
                     throw new RepositoryError(defaultErrorString);
             }
         } catch (IOException | IllegalStateException e) {
-            log.error("Error retrieving response from arXivOAI repository", e);
+            log.error(String.format("Error retrieving response from arXiv OAI repository for request %s", httpRequest.getURI()), e);
             throw new HttpException(e);
         }
 
+    }
+
+    /**
+     * POJO to hold responses from the arXiv OAI repository that we can do something with: either a parsed XML response
+     * or a number of seconds to wait.  Immutable.
+     */
+    @Value
+    private static class RepositoryResponse {
+        ParsedXmlResponse parsedXmlResponse;
+        Duration wait;
     }
 
 }
